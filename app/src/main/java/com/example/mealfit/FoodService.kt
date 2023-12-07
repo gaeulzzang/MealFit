d2368eb05b3ca0e1a6b1ccda81cb5b6d0fcf91b6
1ys <1ys@sookmyung.ac.kr>
2023-12-07 15:47:29 +0900

    식단 추가 버튼, 뒤로 가기 버튼, 메뉴 검색 기능

---
+++ app/src/main/java/com/example/mealfit/FoodService.kt
@@ -0,0 +1,16 @@
+package com.example.mealfit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+//interface FoodService {
+//    @GET("api/{keyId}/{serviceId}/{dataType}/1/5")
+//    fun getFoodInfo(
+//        @Path("keyId") keyId: String,
+//        @Path("serviceId") type: String,
+//        @Path("dataType") dataType: String
+//    ): Call<List<FoodInfo>>
+//}
+
+//"71bdb826d7084a3cb4c7", "I2790", "json"
