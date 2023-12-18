fd14ab821e4299f99acc6115b910e97a83edf5b4
gaeulzzang <gaeulzzang@gmail.com>
2023-12-18 14:20:50 +0900

    listFragment 상태 유지 못해결함

--- app/src/main/java/com/example/mealfit/ListViewModel.kt
+++ app/src/main/java/com/example/mealfit/ListViewModel.kt
@@ -4,4 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class ListViewModel : ViewModel() {
     var isBreakfastVisible: Boolean = false
+    var isLunchVisible: Boolean = false
+    var isDinnerVisible: Boolean = false
 }
